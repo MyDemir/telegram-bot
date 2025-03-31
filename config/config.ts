cca072f0bc1aca5ff86678d3d26fd7a969dcb139
MyD <m.demir-21@outlook.com>
2025-03-31 22:07:25 +0300

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,1 +1,9 @@
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+
+export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN!;
+export const TWITTER_API_KEY = process.env.API_KEY!;
+export const TWITTER_API_SECRET_KEY = process.env.API_SECRET_KEY!;
+export const TWITTER_ACCESS_TOKEN = process.env.ACCESS_TOKEN!;
+export const TWITTER_ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
