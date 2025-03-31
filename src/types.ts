52d3ad82a5a6d4791e9a9a42a7cd35173db90009
MyD <m.demir-21@outlook.com>
2025-03-31 22:06:19 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,18 @@
+export interface UserInfo {
+  [twitterUsername: string]: {
+    last_tweet_id: string | null;
+    chat_id: number;
+    twitter_username: string;
+  } | {
+    source_channel?: number;
+    target_channel?: number;
+  };
+}
 
+export interface Tweet {
+  id: string;
+  full_text: string;
+  user: {
+    screen_name: string;
+  };
+}
