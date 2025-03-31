9c48b993b207e0d275b75b6ec368d7c108eb3e58
MyD <m.demir-21@outlook.com>
2025-03-31 22:09:48 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,18 @@
+import * as TelegramBot from 'node-telegram-bot-api';
+import { TELEGRAM_BOT_TOKEN } from '../config/config';
+import { checkTwitter } from './twitter';
+import { startHandler, setChannelsHandler, addTwitterHandler, forwardContentHandler } from './handlers';
 
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => startHandler(bot, msg));
+bot.onText(/\/set_channels/, (msg) => setChannelsHandler(bot, msg));
+bot.onText(/\/add_twitter/, (msg) => addTwitterHandler(bot, msg));
+bot.on('message', (msg) => {
+  if (!msg.text?.startsWith('/')) forwardContentHandler(bot, msg);
+});
+
+// Twitter kontrolünü her 60 saniyede bir çalıştır
+setInterval(() => checkTwitter(bot), 60 * 1000);
+
+console.log('Bot çalışıyor...');
