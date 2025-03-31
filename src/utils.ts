283b20a0ba91c89e370ce66df7ac1c0fdaefc864
MyD <m.demir-21@outlook.com>
2025-03-31 22:08:06 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,18 @@
+import * as fs from 'fs/promises';
+import { UserInfo } from './types';
 
+export async function loadUserInfo(): Promise<UserInfo> {
+  try {
+    const data = await fs.readFile('user_info.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
+      return {};
+    }
+    throw error;
+  }
+}
+
+export async function saveUserInfo(userInfo: UserInfo): Promise<void> {
+  await fs.writeFile('user_info.json', JSON.stringify(userInfo, null, 4));
+}
