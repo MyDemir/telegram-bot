24bbbb7101dba2be1da2d7d5cdfd8541d5b914d6
MyD <m.demir-21@outlook.com>
2025-03-31 22:26:36 +0300

    Update twitter.ts

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,25 +1,31 @@
+import Twit from 'twit';
 import { TWITTER_API_KEY, TWITTER_API_SECRET_KEY, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET } from '../config/config';
 import { Tweet, UserInfo } from './types';
 import { TelegramBot } from 'node-telegram-bot-api';
 import { loadUserInfo, saveUserInfo } from './utils';
-import * as tweepy from 'tweepy';
 
-const auth = new tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY);
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET);
-const api = new tweepy.API(auth);
+// Twit istemcisini başlat
+const client = new Twit({
+  consumer_key: TWITTER_API_KEY,
+  consumer_secret: TWITTER_API_SECRET_KEY,
+  access_token: TWITTER_ACCESS_TOKEN,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+});
 
 export async function checkTwitter(bot: TelegramBot): Promise<void> {
   const userInfo = await loadUserInfo();
   for (const [twitterUsername, data] of Object.entries(userInfo)) {
     if (!('last_tweet_id' in data)) continue;
     try {
-      const tweets = api.user_timeline({ screen_name: twitterUsername, count: 1, tweet_mode: 'extended' });
+      const params = { screen_name: twitterUsername, count: 1, tweet_mode: 'extended' };
+      const response = await client.get('statuses/user_timeline', params);
+      const tweets = response.data as Tweet[];
       if (tweets.length > 0) {
-        const tweet = tweets[0] as unknown as Tweet;
+        const tweet = tweets[0];
         const lastTweetId = data.last_tweet_id;
-        if (lastTweetId !== tweet.id) {
+        if (lastTweetId !== tweet.id_str) {
           await sendTweetNotification(bot, tweet, data.chat_id);
-          data.last_tweet_id = tweet.id;
+          data.last_tweet_id = tweet.id_str;
           await saveUserInfo(userInfo);
         }
       }
@@ -30,7 +36,7 @@ export async function checkTwitter(bot: TelegramBot): Promise<void> {
 }
 
 async function sendTweetNotification(bot: TelegramBot, tweet: Tweet, chatId: number): Promise<void> {
-  const tweetUrl = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id}`;
+  const tweetUrl = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
   const text = `Yeni tweet! 🐦\n\n${tweet.full_text}\n\n🔗 [Tweeti Görüntüle](${tweetUrl})`;
   await bot.sendMessage(chatId, text, { parse_mode: 'Markdown', disable_web_page_preview: true });
 }
